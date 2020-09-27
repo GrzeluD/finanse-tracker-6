2d2c9ea5afe32274796b1b8782c11815196ef721
Grzegorz Dziedzic <grzegorzdziedzic@onet.pl>
2020-09-27 18:21:02 +0200

    cleanup app layout

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -10,7 +10,7 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
     begin
-      new(ticker:ticker_symbol, name:client.company(ticker_symbol).company_name, last_price:client.price(ticker_symbol), logo:client.logo(ticker_symbol).url)
+      new(ticker:ticker_symbol, name:client.company(ticker_symbol).company_name, last_price:client.price(ticker_symbol))
     rescue => expection
       return nil
     end
