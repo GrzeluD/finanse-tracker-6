3fb076dc3f907acebda1a9edf6393f08c7a6353e
Grzegorz Dziedzic <grzegorzdziedzic@onet.pl>
2020-10-04 17:58:03 +0200

    added user first and last name to db and show in navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_151801) do
+ActiveRecord::Schema.define(version: 2020_10_04_154315) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -29,17 +29,8 @@ ActiveRecord::Schema.define(version: 2020_09_27_151801) do
     t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'strong' for column 'first_name'
 
   add_foreign_key "user_stocks", "stocks"
   add_foreign_key "user_stocks", "users"
